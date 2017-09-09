4d36e9fd23373fa29878addbcfaa485cfaf95613
ATOMIX\Snowy <snowy6@gmail.com>
2017-09-09 09:13:26 +0200

    Colored indexed quad

--- shaders/fragment.fs
+++ shaders/fragment.fs
@@ -1,7 +1,8 @@
 #version 330
 
+in vec4 outColor;
 out vec4 fragColor;
 
 void main() {
-	fragColor = vec4(0.0, 0.5, 0.5, 1.0);
+	fragColor = outColor;
 }
--- src/main/java/org/jacowiese/GLTest.java
+++ src/main/java/org/jacowiese/GLTest.java
@@ -6,6 +6,7 @@
 package org.jacowiese;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import org.jacowiese.shader.Shader;
 import org.jacowiese.util.FileUtils;
 import org.lwjgl.Version;
@@ -14,19 +15,24 @@ import org.lwjgl.glfw.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.opengl.ARBVertexArrayObject.glBindVertexArray;
+import static org.lwjgl.opengl.ARBVertexArrayObject.glDeleteVertexArrays;
 import static org.lwjgl.opengl.ARBVertexArrayObject.glGenVertexArrays;
 import static org.lwjgl.opengl.GL.createCapabilities;
 import org.lwjgl.opengl.GL11;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_FLOAT;
 import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_INT;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.opengl.GL11.glDrawArrays;
+import static org.lwjgl.opengl.GL11.glDrawElements;
+import static org.lwjgl.opengl.GL11.glIndexPointer;
 import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
+import static org.lwjgl.opengl.GL15.GL_ELEMENT_ARRAY_BUFFER;
 import static org.lwjgl.opengl.GL15.GL_STATIC_DRAW;
 import static org.lwjgl.opengl.GL15.glBindBuffer;
 import static org.lwjgl.opengl.GL15.glBufferData;
+import static org.lwjgl.opengl.GL15.glDeleteBuffers;
 import static org.lwjgl.opengl.GL15.glGenBuffers;
 import static org.lwjgl.opengl.GL20.glDisableVertexAttribArray;
 import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
@@ -46,15 +52,32 @@ public class GLTest {
     private Shader myShader;
 
     private float[] vertices = new float[]{
-        0.0f, 0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
+        -1f, 1f, 0.0f,
+        -1f, -1f, 0.0f,
+        1f, -1f, 0.0f,
+        1f, 1f, 0.0f
+    };
+
+    private float[] colors = new float[]{
+        1.0f, 0.0f, 0.0f, 1.0f,
+        0.0f, 1.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 0.0f, 1.0f
+    };
+
+    private int[] indices = new int[]{
+        0, 1, 2,
+        0, 2, 3
     };
 
     private int vaoId;
     private int vboId;
+    private int iboId;
+    private int colorvboId;
 
     FloatBuffer verticesBuffer;
+    IntBuffer indicesBuffer;
+    FloatBuffer colorBuffer;
 
     private void init() throws Exception {
         System.setProperty("org.lwjgl.util.Debug", "true");
@@ -86,16 +109,19 @@ public class GLTest {
 
         createCapabilities();
 
+        // Create a shader
         myShader = new Shader();
         myShader.createVertexShader(FileUtils.loadResource("shaders/vertex.vs"));
         myShader.createFragmentShader(FileUtils.loadResource("shaders/fragment.fs"));
         myShader.link();
 
-        verticesBuffer = MemoryUtil.memAllocFloat(vertices.length);
-        verticesBuffer.put(vertices).flip();
-
+        // Set up the vao
         vaoId = glGenVertexArrays();
         glBindVertexArray(vaoId);
+
+        // Set up the vertex buffer
+        verticesBuffer = MemoryUtil.memAllocFloat(vertices.length);
+        verticesBuffer.put(vertices).flip();
 
         vboId = glGenBuffers();
         glBindBuffer(GL_ARRAY_BUFFER, vboId);
@@ -103,6 +129,46 @@ public class GLTest {
         memFree(verticesBuffer);
 
         glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+
+        // Unbind the VBO
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        if (verticesBuffer != null) {
+            MemoryUtil.memFree(verticesBuffer);
+        }
+
+        // Set up in the index buffer
+        indicesBuffer = MemoryUtil.memAllocInt(indices.length);
+        indicesBuffer.put(indices).flip();
+
+        iboId = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+        memFree(indicesBuffer);
+
+        glIndexPointer(4, indicesBuffer);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        // Set up the color buffer
+        colorBuffer = MemoryUtil.memAllocFloat(colors.length);
+        colorBuffer.put(colors).flip();
+
+        colorvboId = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, colorvboId);
+        glBufferData(GL_ARRAY_BUFFER, colorBuffer, GL_STATIC_DRAW);
+        memFree(colorBuffer);
+
+        glVertexAttribPointer(1, 4, GL_FLOAT, false, 0, 0);
+
+        // Unbind the colorVBO
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        if (colorBuffer != null) {
+            MemoryUtil.memFree(colorBuffer);
+        }
+
+        // Unbind the VAO
+        glBindVertexArray(0);
+
     }
 
     private void loop() {
@@ -116,10 +182,13 @@ public class GLTest {
 
             // Bind to the VAO
             glBindVertexArray(vaoId);
-            glEnableVertexAttribArray(0);
+            glEnableVertexAttribArray(0); // Vertex data
+            glEnableVertexAttribArray(1); // Color data
 
             // Draw the vertices
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            //glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, 3);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
             // Restore state
             glDisableVertexAttribArray(0);
             glBindVertexArray(0);
@@ -133,13 +202,17 @@ public class GLTest {
     }
 
     private void destroy() {
-        // Unbind the VBO
+        glDisableVertexAttribArray(0);
+
+        // Delete the vbos
         glBindBuffer(GL_ARRAY_BUFFER, 0);
-        // Unbind the VAO
+        glDeleteBuffers(vboId);
+        glDeleteBuffers(iboId);
+        glDeleteBuffers(colorvboId);
+
+        // Delete the vao
         glBindVertexArray(0);
-        if (verticesBuffer != null) {
-            MemoryUtil.memFree(verticesBuffer);
-        }
+        glDeleteVertexArrays(vaoId);
 
         myShader.cleanup();
 
